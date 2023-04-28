82c4471065807763858d1bac6e7d26a11968072b
developerhasan99 <92197261+developerhasan99@users.noreply.github.com>
2023-04-28 02:04:00 +0000

    Deploying to gh-pages from @ developerhasan99/developerhasan99.github.io@0b018d117147a71b8d9406153bc54861e3b3abcb 🚀

--- _next/static/YoC9DGA1TxJglCpmh-3Rj/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/75fc9c18-289ba7b5fb63f228.js","static/chunks/703-c8b0052b29d4ea16.js","static/chunks/pages/index-39dfce2cc5742882.js"],"/_error":["static/chunks/pages/_error-777a5f0e462a9bdf.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
